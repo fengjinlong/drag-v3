4883bf3a9616dcbda022e77e6d4919f546b67c6b
fjl <18618166564@163.com>
2022-02-22 17:38:10 +0800

    quick

--- 1.js
+++ 1.js
@@ -1,9 +1,1 @@
-let el = Array.from(document.getElementByTagName("*"))
-let elObj = {}
-let reg = /^[h|s].+/gi
-el.map(item => {
- const tagName = item.tagName
- if(reg.tast(tagName)){
-    !elObj[tagName] ? elIbj[tagName] = 1 : elObj[tagName]
-  }
-})
+
